d9015d65c1eb3fe7ed139bbe4b698766f41ca256
JOANR-CEO <joanr@example.com>
2026-03-10 03:37:16 -0500

    🩹 Operación de Rescate: Simplificación total de API

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 ﻿from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get("/api/comandos")
-def read_root():
-    return {"status": "Sistema Los Parceritos Online", "comando": None}
+def get_cmd():
+    return {"status": "Sistema Online", "comando": None}
 
 @app.post("/api/enviar")
-async def enviar(data: dict):
+async def post_cmd(data: dict):
     return {"status": "Recibido", "data": data}
